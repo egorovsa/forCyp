411347eb09bfa3ad13e4c54268d57e76191f939e
egorovsa <design@192.168.1.105>
2016-02-12 16:27:29 +0300

    ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,7 @@
 var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
-var ngmin = require('gulp-ngmin');
 var rename = require('gulp-rename');
-var replace = require('gulp-replace');
-var sass = require('gulp-sass');
-var minifyHtml = require('gulp-minify-html');
-var minifyCss = require('gulp-minify-css');
 var connect = require('gulp-connect');
-var notify = require("gulp-notify");
 var uglify = require('gulp-uglify');
-var ngAnnotate = require('gulp-ng-annotate');
-var embedTemplates = require('gulp-angular-embed-templates');
-var templateCache = require('gulp-angular-templatecache');
 
 //connect
 gulp.task('connect', function () {
@@ -31,15 +20,11 @@ gulp.task('html', function () {
 //js
 gulp.task('js', function () {
     gulp.src('src/js/*.js')
-//            .pipe(embedTemplates())
-//            .pipe(ngAnnotate())
             .pipe(uglify())
             .pipe(rename({
                 suffix: ".min"
             }))
-            //.pipe(rename('app.min.js'))
             .pipe(gulp.dest('js/'))
-            //.pipe(notify('JS task is done'))
             .pipe(connect.reload());
 });
 
@@ -47,7 +32,6 @@ gulp.task('js', function () {
 //watch
 gulp.task('watch', function () {
     gulp.watch('*.html', ['html']);
-    gulp.watch('public/src/sass/{,*/*/}*.scss', ['sass']);
     gulp.watch('src/js/*.js', ['js']);
 });
 
